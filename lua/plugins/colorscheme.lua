e24c2c80f1d48925081496fd5f1ffb104d2ca637
benjamin <32769527+benjaminjellis@users.noreply.github.com>
2024-12-01 18:56:07 +0000

    feat: turn off unused lazyvim plugins

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,6 @@
 return {
+  { "folke/tokyonight.nvim", enabled = false },
+  { "catppuccin/nvim", enabled = false },
   {
     "benjaminjellis/citrusdream.nvim",
     name = "citrusdream",
---
+++ lua/plugins/snippets.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "garymjr/nvim-snippets",
+    enabled = false,
+  },
+
+  { "rafamadriz/friendly-snippets", enabled = false },
+}
