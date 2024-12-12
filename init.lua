992408a5fc9d8a9a536a2ed8fa57c6b25165e52a
benjamin <32769527+benjaminjellis@users.noreply.github.com>
2024-12-12 11:43:35 +0000

    feat: remove non-lazy set up of typos lsp

--- init.lua
+++ init.lua
@@ -1,17 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
--- TODO: figure out why this only seems to work here
-require("lspconfig").typos_lsp.setup({
-  init_options = {
-    -- Equivalent to the typos `--config` cli argument.
-    config = "~/.config/typos.toml",
-    -- How typos are rendered in the editor, can be one of an Error, Warning, Info or Hint.
-    -- Defaults to error.
-    diagnosticSeverity = "Hint",
-    settings = {
-      -- Another way to specify words to ignore
-      ignore_words = { "noice" },
-    },
-  },
-})
--- lua/plugins/spellchecking.lua
+++ lua/plugins/spellchecking.lua
@@ -5,12 +5,4 @@ return {
       vim.list_extend(opts.ensure_installed or {}, { "typos-lsp" })
     end,
   },
-  {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {},
-    },
-  },
 }
