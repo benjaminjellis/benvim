1121b6a2827349843f65192a2ef3193c311987e1
benjamin <32769527+benjaminjellis@users.noreply.github.com>
2024-12-13 12:24:48 +0000

    feat: remvoe unused keymaps for speluker

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,9 +1,2 @@
 vim.keymap.set("n", ";", ":", { remap = true })
 vim.keymap.set("n", "<leader>cb", "<cmd>%bd|e#<cr>")
--- spelling
-vim.keymap.set("n", "<leader>spn", function()
-  vim.fn["spelunker#jump_next"]()
-end, { desc = "Go to next mis-spelled word", remap = true })
-vim.keymap.set("n", "<leader>spp", function()
-  vim.fn["spelunker#jump_prev"]()
-end, { desc = "Go to previous mis-spelled word", remap = true })
