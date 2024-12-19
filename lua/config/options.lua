59d171cc9eb0783cf35b4fecd3110ede62e02af7
benjamin <32769527+benjaminjellis@users.noreply.github.com>
2024-12-19 10:42:29 +0000

    feat: use blink main to avoid input completion bug

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,5 @@
 -- Add any additional options here
 vim.opt.swapfile = false
 vim.g.snacks_animate = false
+-- HACK: use blink from main to get around the blink providing suggestions for renaming
+vim.g.lazyvim_blink_main = true
