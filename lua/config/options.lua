68bff7b86e2d5b37f1260a164cbeec08bdd5833b
benjamin <32769527+benjaminjellis@users.noreply.github.com>
2024-12-21 14:11:06 +0000

    feat: remove blink main hack

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,5 +3,3 @@
 -- Add any additional options here
 vim.opt.swapfile = false
 vim.g.snacks_animate = false
--- HACK: use blink from main to get around the blink providing suggestions for renaming
-vim.g.lazyvim_blink_main = true
