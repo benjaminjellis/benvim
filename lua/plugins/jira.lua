ffd0d5f158781ec71b7628f28f4dc9f22ac0c073
benjamin <32769527+benjaminjellis@users.noreply.github.com>
2024-12-30 21:33:12 +0000

    feat: update jirac and rustacean vim

--- lua/plugins/jira.lua
+++ lua/plugins/jira.lua
@@ -1,11 +1,36 @@
+local jira_email = os.getenv("JIRA_EMAIL")
+local jira_domain = os.getenv("JIRA_DOMAIN")
+local jira_api_token = os.getenv("JIRA_API_TOKEN")
+local jira_project_key = os.getenv("JIRA_PROJECT_KEY")
+
+local function maybe_set_up_jira()
+  if jira_email and jira_domain and jira_api_token and jira_project_key then
+    return {
+      "janBorowy/jirac.nvim",
+      event = "VeryLazy",
+      dependencies = {
+        "MunifTanjim/nui.nvim",
+        "grapp-dev/nui-components.nvim",
+        "nvim-lua/plenary.nvim",
+      },
+      opts = {
+        email = jira_email,
+        jira_domain = jira_domain,
+        api_key = jira_api_token,
+        config = {
+          default_project_key = jira_project_key,
+          keymaps = {},
+          window_width = 150,
+          window_height = 50,
+        },
+      },
+    }
+  else
+    print("Expecting env vars to be set: JIRA_EMAIL, JIRA_DOMAIN, JIRA_API_TOKEN, JIRA_PROJECT_KEY. One was not set")
+    return {}
+  end
+end
+
 return {
-  {
-    "janBorowy/jirac.nvim",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "grapp-dev/nui-components.nvim",
-      "nvim-lua/plenary.nvim",
-    },
-    opts = function() end,
-  },
+  maybe_set_up_jira(),
 }
