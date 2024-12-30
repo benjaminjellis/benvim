7629e5fc44535404e54e3f061c642b48dcd7cde7
benjamin <32769527+benjaminjellis@users.noreply.github.com>
2024-12-30 20:47:42 +0000

    feat: try out jira

---
+++ lua/plugins/jira.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "janBorowy/jirac.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "grapp-dev/nui-components.nvim",
+      "nvim-lua/plenary.nvim",
+    },
+    opts = function() end,
+  },
+}
