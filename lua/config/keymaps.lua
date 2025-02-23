b202706abb7a90641a6c7dd65fcfd6dc3a73f9f2
benjamin <32769527+benjaminjellis@users.noreply.github.com>
2025-02-23 18:30:36 +0000

    feat: update parrot keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,2 +1,2 @@
-vim.keymap.set("n", ";", ":", { remap = true })
+vim.keymap.set({ "n", "v" }, ";", ":", { remap = true })
 vim.keymap.set("n", "<leader>cb", "<cmd>%bd|e#<cr>")
---
+++ lua/plugins/ai.lua
@@ -0,0 +1,23 @@
+return {
+  "frankroeder/parrot.nvim",
+  event = "VeryLazy",
+  dependencies = { "ibhagwan/fzf-lua", "nvim-lua/plenary.nvim" },
+  opts = { providers = { gemini = {
+    api_key = os.getenv("GEMINI_API_KEY"),
+  } } },
+  keys = {
+    { "<leader>ac", "<cmd>PrtChatToggle<cr>", desc = "🦜 Open Parrot Chat" },
+    { "<leader>ar", "<cmd>PrtChatRespond<cr>", desc = "🦜 Respond to Parrot Chat" },
+    { "<leader>as", "<cmd>PrtChatStop<cr>", desc = "🦜 Interrupt ongoing Parrot respond" },
+    { "<leader>ad", "<cmd>PrtChatDelete<cr>", desc = "🦜 Delete the current chat file" },
+    { "<leader>aa", ":PrtAppend ", desc = "🦜 Use Parrot to append code", mode = { "v" } },
+    { "<leader>ar", ":PrtRewrite ", desc = "🦜 Use Parrot to rewrite code", mode = { "v" } },
+    { "<leader>ap", ":PrtPrepend", desc = "🦜 Use Parrot to prepend code", mode = { "v" } },
+    {
+      "<leader>ad",
+      ":PrtPrepend write docs for this function or type<cr>",
+      desc = "🦜 Use Parrot to write docs for function or type",
+      mode = { "v" },
+    },
+  },
+}
