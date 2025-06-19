a5d09e4af5bc5d9a8b3f63f83bcb5c2be3424fd5
benjamin <32769527+benjaminjellis@users.noreply.github.com>
2025-06-19 15:39:46 +0100

    fix: start up time regression

--- lua/plugins/spellchecking.lua
+++ lua/plugins/spellchecking.lua
@@ -7,42 +7,50 @@ return {
   -- },
   {
     "williamboman/mason.nvim",
+    lazy = true,
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, { "harper-ls" })
     end,
-    init = function()
-      require("lspconfig").harper_ls.setup({
-        settings = {
-          ["harper-ls"] = {
-            userDictPath = "",
-            fileDictPath = "",
-            linters = {
-              SpellCheck = true,
-              SpelledNumbers = false,
-              AnA = true,
-              SentenceCapitalization = false,
-              UnclosedQuotes = true,
-              WrongQuotes = false,
-              LongSentences = true,
-              RepeatedWords = true,
-              Spaces = true,
-              Matcher = true,
-              CorrectNumberSuffix = true,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = true,
+    opts = {
+      servers = {
+        harper_ls = {
+
+          settings = {
+            ["harper-ls"] = {
+              userDictPath = "",
+              fileDictPath = "",
+              linters = {
+                SpellCheck = true,
+                SpelledNumbers = false,
+                AnA = true,
+                SentenceCapitalization = true,
+                UnclosedQuotes = true,
+                WrongQuotes = false,
+                LongSentences = true,
+                RepeatedWords = true,
+                Spaces = true,
+                Matcher = true,
+                CorrectNumberSuffix = true,
+              },
+              codeActions = {
+                ForceStable = false,
+              },
+              markdown = {
+                IgnoreLinkTitle = false,
+              },
+              diagnosticSeverity = "hint",
+              isolateEnglish = false,
+              dialect = "American",
+              maxFileLength = 120000,
             },
-            codeActions = {
-              ForceStable = false,
-            },
-            markdown = {
-              IgnoreLinkTitle = false,
-            },
-            diagnosticSeverity = "hint",
-            isolateEnglish = false,
-            dialect = "British",
-            maxFileLength = 120000,
           },
         },
-      })
-    end,
+      },
+    },
   },
 }
