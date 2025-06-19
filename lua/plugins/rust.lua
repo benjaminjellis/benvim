d0733a621b090ced98372534797413a81bfd703b
benjamin <32769527+benjaminjellis@users.noreply.github.com>
2025-06-19 15:13:32 +0100

    feat: general clean up, drop rarely used langs configs try using harper for spell

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -34,7 +34,6 @@ return {
   -- Ensure Rust debugger is installed
   {
     "williamboman/mason.nvim",
-    optional = true,
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, { "codelldb" })
--- lua/plugins/spellchecking.lua
+++ lua/plugins/spellchecking.lua
@@ -1,8 +1,48 @@
 return {
+  -- {
+  --   "williamboman/mason.nvim",
+  --   opts = function(_, opts)
+  --     vim.list_extend(opts.ensure_installed or {}, { "typos-lsp" })
+  --   end,
+  -- },
   {
     "williamboman/mason.nvim",
     opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed or {}, { "typos-lsp" })
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "harper-ls" })
+    end,
+    init = function()
+      require("lspconfig").harper_ls.setup({
+        settings = {
+          ["harper-ls"] = {
+            userDictPath = "",
+            fileDictPath = "",
+            linters = {
+              SpellCheck = true,
+              SpelledNumbers = false,
+              AnA = true,
+              SentenceCapitalization = true,
+              UnclosedQuotes = true,
+              WrongQuotes = false,
+              LongSentences = true,
+              RepeatedWords = true,
+              Spaces = true,
+              Matcher = true,
+              CorrectNumberSuffix = true,
+            },
+            codeActions = {
+              ForceStable = false,
+            },
+            markdown = {
+              IgnoreLinkTitle = false,
+            },
+            diagnosticSeverity = "hint",
+            isolateEnglish = false,
+            dialect = "American",
+            maxFileLength = 120000,
+          },
+        },
+      })
     end,
   },
 }
