1142490bf8e67b8247c3795ee8bbd876b7fcf6db
benjaminjellis <benjaminjellis@protonmail.com>
2025-07-13 12:21:16 +0100

    feat: update colour scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,26 +1,11 @@
 return {
+  { "RostislavArts/naysayer.nvim", name = "naysayer" },
+  { "benjaminjellis/ns.nvim", name = "ns" },
   { "benjaminjellis/everforest-nvim", name = "everforest", opts = { ui_contrast = "high", background = "soft" } },
-  {
-    "benjaminjellis/rose-pine",
-    name = "rose-pine",
-    opts = {
-      variant = "main",
-      styles = {
-        transparency = true,
-      },
-      -- these options make the theme transparent
-      disable_background = true,
-      disable_float_background = true,
-      highlight_groups = {
-        Normal = { bg = "none" },
-      },
-    },
-  },
-  { "catppuccin/nvim", enabled = false },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "everforest",
+      colorscheme = "ns",
     },
   },
 }
